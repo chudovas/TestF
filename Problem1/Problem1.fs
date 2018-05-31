2a19024e7c367c5c84c61c56e1db707b5789f90e
Гурьев Василий Александрович <Vaschudo@gmail.com>
2018-05-31 12:17:46 +0300

    Problems complite!

--- Problem1/Problem1.fs
+++ Problem1/Problem1.fs
@@ -1,8 +1,18 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+let sumFib maxNum =
+    let rec fib l max =
+        match l with
+        | [] -> fib (1::l) max
+        | [a] -> fib (1::l) max
+        | _ when ((List.head l) + List.head(List.tail l)) < max -> 
+            fib (((List.head l) + List.head(List.tail l))::l) max
+        | _ -> l
+    (fib [] maxNum) |> List.filter (fun a -> a % 2 = 0) |> List.sum
+
+let sumFibBillion =
+    sumFib 1000000
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    0
--- Problem3/Problem3.fs
+++ Problem3/Problem3.fs
@@ -1,8 +1,24 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+type Stack() =
+    let mutable stack = []
+    let push(el) =
+        stack <- el::stack
+
+    let isEmpty() =
+        stack |> List.isEmpty
+
+    let pop() =
+        match stack with
+        | []   -> (raise (ArgumentException("Empty stack!")))
+        | a::b -> 
+            stack <- b
+            a    
+
+    member this.Push(el) = push(el)
+    member this.Pop() = pop()
+    member this.IsEmpty() = isEmpty()
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    0 
